77c7171f32a4102e471304b4220c84feec081aa5
Abdulmatin Sanni <me@abdulmatinsanni.com>
2018-04-07 01:09:33 +0100

    Added index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <title>LASUDev1874 | Collaborative Coding using VCS - Git</title>
+  <meta name="author" content="Abdumatin Sanni">
+  <meta name="description" content="LASUDev1874 Collaborative Coding using VCS - Git">
+  <meta charset="utf-8">
+
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+</body>
+</html>
